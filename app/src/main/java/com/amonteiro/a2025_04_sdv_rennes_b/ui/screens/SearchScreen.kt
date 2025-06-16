f2941842eca725789ea20082c0ff87e0141ddfcf
Anthony <anth06ny@gmail.com>
2025-06-16 16:51:46 +0200

    Avant TP Compose Donnée

--- app/src/main/java/com/amonteiro/a2025_04_sdv_rennes_b/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a2025_04_sdv_rennes_b/ui/screens/SearchScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.amonteiro.a2025_04_sdv_rennes_b.ui.theme._2025_04_sdv_rennes_bTheme
+import com.amonteiro.a2025_04_sdv_rennes_b.viewmodel.MainViewModel
 
 @Preview(showBackground = true, showSystemUi = true)
 @Preview(showBackground = true, showSystemUi = true,
@@ -23,21 +25,25 @@ fun SearchScreenPreview() {
     //Utilisé par exemple dans MainActivity.kt sous setContent {...}
     _2025_04_sdv_rennes_bTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+
             SearchScreen(modifier = Modifier.padding(innerPadding))
         }
     }
 }
 
 @Composable
-fun SearchScreen(modifier: Modifier = Modifier) {
+fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
     Column (modifier= modifier) {
         println("SearchScreen()")
         Text(text = "Text1",fontSize = 20.sp)
         Spacer(Modifier.size(8.dp))
         Text(text = "Text2",fontSize = 14.sp)
 
-        repeat(10) {
-            PictureRowItem("Coucou$it")
+        val list = mainViewModel.dataList.collectAsStateWithLifecycle().value
+
+        repeat(list.size) {
+            PictureRowItem(list[it].title)
         }
     }
 }
--- app/src/main/java/com/amonteiro/a2025_04_sdv_rennes_b/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/amonteiro/a2025_04_sdv_rennes_b/viewmodel/MainViewModel.kt
@@ -26,6 +26,10 @@ class MainViewModel : ViewModel() {
     val runInProgress = MutableStateFlow(false)
     val errorMessage = MutableStateFlow("")
 
+    init {
+        loadFakeData()
+    }
+
     fun loadWeathers(cityName: String) {
 
         runInProgress.value = true
@@ -59,4 +63,20 @@ class MainViewModel : ViewModel() {
         }
 
     }
+
+    fun loadFakeData(runInProgress :Boolean = false, errorMessage:String = "" ) {
+        this.runInProgress.value = runInProgress
+        this.errorMessage.value = errorMessage
+        dataList.value = listOf(PictureBean(1, "https://picsum.photos/200", "ABCD", LONG_TEXT),
+            PictureBean(2, "https://picsum.photos/201", "BCDE", LONG_TEXT),
+            PictureBean(3, "https://picsum.photos/202", "CDEF", LONG_TEXT),
+            PictureBean(4, "https://picsum.photos/203", "EFGH", LONG_TEXT)
+        ).shuffled() //shuffled() pour avoir un ordre différent à chaque appel
+    }
 }
+
+
+
+const val LONG_TEXT = """Le Lorem Ipsum est simplement du faux texte employé dans la composition
+    et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard
+    de l'imprimerie depuis les années 1500"""
